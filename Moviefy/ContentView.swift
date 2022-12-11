e10f98c1f2f83f2f8463e7acba8f2eaea3ee92ad
subfly <98735584+alitahabundle@users.noreply.github.com>
2022-12-11 20:31:56 +0300

    Hope this works

--- Moviefy/ContentView.swift
+++ Moviefy/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Fefe")
+            Text("Sese")
         }
         .padding()
     }
