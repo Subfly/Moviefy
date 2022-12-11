e74eaf8ba0fe4c9bc2bf2127af81408a637454d2
alitahabundle <98735584+alitahabundle@users.noreply.github.com>
2022-12-11 19:32:03 +0300

    Initial Commit

---
+++ Moviefy/MoviefyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviefyApp.swift
+//  Moviefy
+//
+//  Created by Ali Taha Dinçer on 11.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoviefyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
